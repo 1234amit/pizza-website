2dd22c848c0185118bc2e69292bd71d83869d332
Amit Goswami <cseiubatgoswami@gmail.com>
2021-04-01 19:10:51 +0600

    first commit

---
+++ src/components/Footer/FooterElements.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const FooterContainer = styled.div `
+    justify-content: center;
+    text-align: center;
+    padding: 30px;
+    background: black;
+    color: #fff;
+`;
+
+
+export const FooterHeader = styled.div `
+    h1{
+        color:#fff;
+    }
+
+`;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FooterContainer, FooterHeader } from './FooterElements'
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterHeader>
+                <h2>All Copywrite @2021 Amit Goswami</h2>
+            </FooterHeader>
+        </FooterContainer>
+    )
+}
+
+export default Footer
